f9081162febb7d794cb10f4a3584378e99e56db0
Addison Nance <madi.nance911@gmail.com>
2020-03-14 14:04:00 -0600

    javascript coding. Defining vairables, defining key capture functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,29 @@
 // Array that defines the words and selects a random word
-var gameArray = ["word1", "word2","word3","word4"];
-    var randomWord = gameArray[Math.floor(Math.random() *gameArray.length)];
-    document.body.innerhtml = randomWord;
+var gameArray = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var randomLetter = gameArray[Math.floor(Math.random() *gameArray.length)];
+    console.log(randomLetter)
+
+    var gameWins=0
+    var gameLosses=0
+    var guessesLeft=15
+
+    document.getElementById("guessesRemains").innerHTML=(guessesLeft)
+
+    document.onkeyup=function(event){
+        var letter = event.key.toLowerCase();
+
+if (letter === randomLetter){
+    console.log("true");
+    gameWins++
+    document.getElementById("wins").innerHTML = gameWins
+    // document.randomLetter.reset();
+
+}else if(guessesLeft=0) {
+    console.log("GameOver");
+
+} else{
+    guessesLeft--
+    document.getElementById("previousGuesses").prepend(" " + letter);
+
+}
+    }
